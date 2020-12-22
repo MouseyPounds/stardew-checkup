82963075ec5e20edd0663cc3bc6e727934928d2a
Thorsten Essig <te@tholutions.de>
2020-12-22 12:58:14 +0100

    fix errors on multiplayer file parsing

--- stardew-checkup.js
+++ stardew-checkup.js
@@ -1530,6 +1530,7 @@ window.onload = function () {
 			meta.isHost = false;
 			$(xmlDoc).find('farmhand').each(function () {
 				if (isValidFarmhand(this)) {
+					let wedding = Number($(xmlDoc).find('countdownToWedding').text());
 					table.push(parsePlayerFamily(this, saveInfo, wedding, meta));
 				}
 			});
@@ -3177,7 +3178,7 @@ window.onload = function () {
 		if (saveInfo.numPlayers > 1) {
 			$(xmlDoc).find('farmhand').each(function () {
 				if (isValidFarmhand(this)) {
-					table.push(parsePlayerQuests(this, saveInfo));
+					table.push(parsePlayerQuests(this, saveInfo, meta));
 				}
 			});
 		}
