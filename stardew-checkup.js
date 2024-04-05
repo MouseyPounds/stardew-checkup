fb47cb2b71c0590d48cacb77d74dab39a90c4d0c
ThatGamerBlue <thatgamerblue@gmail.com>
2024-04-05 03:12:38 +0100

    fix golden walnuts more info linking to skills

--- stardew-checkup.js
+++ stardew-checkup.js
@@ -4863,7 +4863,7 @@ window.onload = function () {
 				getPerfectionPctString(pct["Fishing"], 10, 'Fish Caught', 0, best["Fishing"]) + ' -- <a href="#sec_Fishing">see above for needs</a>';
 		output += '</li><li>';
 		output += (pct["Walnuts"] >= 1) ? getPerfectionPctNumString(pct["Walnuts"], 5, 130, 'Golden Walnuts Found', 1) :
-				getPerfectionPctNumString(pct["Walnuts"], 5, 130, 'Golden Walnuts Found', 0) + ' -- <a href="#sec_Skills">see above for needs</a>';
+				getPerfectionPctNumString(pct["Walnuts"], 5, 130, 'Golden Walnuts Found', 0) + ' -- <a href="#sec_Golden_Walnuts">see above for needs</a>';
 		if (waivers > 0) {
 			output += '</li><li><span class="pt_yes"><span class="pts">' + waivers + '%</span> from purchase of ' + waivers +
 				' Perfection Waivers</span>';
@@ -4949,7 +4949,7 @@ window.onload = function () {
 				getPerfectionPctString(pct["Fishing"], 10, 'Fish Caught', 0) + ' -- <a href="#sec_Fishing">see above for needs</a>';
 		output += '</li><li>';
 		output += (pct["Walnuts"] >= 1) ? getPerfectionPctNumString(pct["Walnuts"], 5, 130, 'Golden Walnuts Found', 1) :
-				getPerfectionPctNumString(pct["Walnuts"], 5, 130, 'Golden Walnuts Found', 0) + ' -- <a href="#sec_Skills">see above for needs</a>';
+				getPerfectionPctNumString(pct["Walnuts"], 5, 130, 'Golden Walnuts Found', 0) + ' -- <a href="#sec_Golden_Walnuts">see above for needs</a>';
 		output += '</li></ul></div>';
 		table.push(output);
 
